21d14d90dbc74c198f93c4b9e34f1978ca56efd1
Taku <yabu.taku.spass@gmail.com>
2020-05-07 16:45:48 +0900

    プログラム第一弾！これからデバグ

--- src/my_random_action_client.py
+++ src/my_random_action_client.py
@@ -4,8 +4,39 @@
 import rospy
 
 import time
+import sys
 import actionlib
 #自分の作ったアクションファイル名がメッセージの型として自動生成される
-from basics.msg import my-timerAction, my-timerGoal, my-timerResult, my-timerFeedback
+from basics.msg import mytimerAction, mytimerGoal, mytimerResult, mytimerFeedback
 
-def feedback
+def feedback_mytimer(feedback):
+    print('[Feedback time] Time elapsed: %f'%(feedback.time_elapsed.to_sec()))
+    print('[Feedback number] Recent number: %d'%(feedback.recent_number))
+    print('[Feedback evenNo.] Even number appearance: %d'%(feedback.even_number))
+
+rospy.init_node('my_timer_action_client')
+client = actionlib.SimpleActionClient('timer',mytimerAction)
+client.wait_for_server()
+
+goal = mytimerGoal()
+numb_get = sys.argv[1:]
+goal.end_number = int(''.join(numb_get))
+
+client.send_goal(goal, feedback_mytimer=feedback_mytimer)
+
+client.wait_for_result()
+
+print('[Result] State: %d'%(client.get_state())) #１〜４までの数字で状態を表示
+print('[Result] Status: %s'%(client.get_goal_status_text()))#サーバー側で設定した文字を表示
+print('[Result time] Time elapsed: %f'%(client.get_result().time_elapsed.to_sec()))
+sent_updates = client.get_result().updates_sent
+print('[Result updates] Updates sent: %d'%(sent_updates))
+number_even = client.get_result.even_number
+print('[Result evenNo.] Even number appearance: %d'%(number_even))
+
+if sent_updates == 0:
+    ratio_even = 0
+else:
+    ratio_even = float(number_even) / float(sent_updates)
+
+print('[Result ratio] Ratio of even number: %f'%(ratio_even*100.0))
--- src/my_random_action_server.py
+++ src/my_random_action_server.py
@@ -2,8 +2,59 @@
 # BEGIN ALL
 #! /usr/bin/env python
 import rospy
-
+from random import random
 import time
 import actionlib
 #自分の作ったアクションファイル名がメッセージの型として自動生成される
 from basics.msg import mytimerAction, mytimerGoal, mytimerResult, mytimerFeedback
+
+def do_timer(goal)
+    start_time = time.time()
+    update_count = 0
+    even_count = 0
+
+    rand_number = 0
+
+    if goal.end_number > 10:
+        if goal.end_number < 1:
+            result = mytimerResult()
+            result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
+            result.updates_sent = update_count
+            result.even_number = even_count
+            server.set_aborted(result,"Action aborted due to too small or large number")
+            return
+        
+    while rand_number != goal.end_number:
+            
+        rand_number = random.randint(1,10)
+        if rand_number%2 == 0
+            even_count += 1
+
+        if server.is_preempt_requested():
+            result = mytimerResult()
+            result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
+            result.updates_sent = update_count
+            result.even_number = even_count
+            server.set_preempted(result, "Action preempted")
+        return
+
+        feedback = mytimerFeedback()
+        feedback.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
+        feedback.recent_number = rand_number
+        feedback.even_number = even_count
+        server.publish_feedback(feedback)
+        
+        update_count += 1
+
+        time.sleep(1.0)
+
+    result = mytimerResult()
+    result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
+    result.updates_sent = update_count
+    result.even_number = even_count
+    server.set_succeeded(result,"Action completed successfully")
+
+rospy.init_node('my_timer_action_server')
+server = actionlib.SimpleActionServer('timer',mytimerAction,do_timer,False)
+server.start()
+rospy.spin()
